219956438155435219477726950be4ef2867c57a
ViktoriyaTarasevich <viktoriya.v.tarasevich@gmail.com>
2015-10-08 09:49:55 +0300

    [Update] Added the title of project.

--- ALPR/ALPR/Form1.Designer.cs
+++ ALPR/ALPR/Form1.Designer.cs
@@ -177,6 +177,7 @@
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
+            this.Text = "Распознование автомобильных номеров";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
